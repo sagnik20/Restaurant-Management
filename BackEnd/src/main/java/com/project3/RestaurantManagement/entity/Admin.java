26edc97914a873dbcc5eb505868170b7bac18081
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2022-05-11 14:29:19 +0530

    admin Entity initialised

---
+++ BackEnd/src/main/java/com/project3/RestaurantManagement/entity/Admin.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.project3.RestaurantManagement.entity;
+
+import com.project3.RestaurantManagement.entityInterface.EntityInterface;
+
+/**
+ * @author sagnik
+ *
+ */
+public class Admin implements EntityInterface {
+
+}
---
+++ BackEnd/src/main/java/com/project3/RestaurantManagement/entityInterface/EntityInterface.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.project3.RestaurantManagement.entityInterface;
+
+/**
+ * @author sagnik
+ *
+ */
+public interface EntityInterface {
+
+}
