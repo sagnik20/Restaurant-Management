75b1ceb7b1ec486ab83f9d133db12c1352d6909f
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2022-05-11 13:37:48 +0530

    initial

---
+++ src/test/java/com/project3/RestaurantManagement/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project3.RestaurantManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
